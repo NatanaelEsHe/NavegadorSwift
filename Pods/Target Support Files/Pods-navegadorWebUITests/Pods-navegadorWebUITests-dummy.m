2da77d607f88b2e491b27d983402d0ebb6bef085
Abrahzan <abrahzan@Mac-mini-de-Abrazan.local>
2017-11-06 16:41:39 +0100

    mi primer commit

---
+++ Pods/Target Support Files/Pods-navegadorWebUITests/Pods-navegadorWebUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_navegadorWebUITests : NSObject
+@end
+@implementation PodsDummy_Pods_navegadorWebUITests
+@end
--- navegadorWeb/ViewController.swift
+++ navegadorWeb/ViewController.swift
@@ -7,19 +7,75 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bAtras: UIButton!
+    @IBOutlet weak var bSiguiente: UIButton!
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var tfNombre: UITextField!
+    var ref:DatabaseReference?
+    @IBOutlet weak var bAdd: UIButton!
+    @IBOutlet weak var tabla: UITableView!
+    var handle:DatabaseHandle?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ref = Database.database().reference()
+        
+        tfNombre.addTarget(self, action: #selector(tfAction(_:)), for: .editingChanged)
+        bAdd.isEnabled=false
+        bAtras.isEnabled=false
+        bSiguiente.isEnabled=false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func agregar(_ sender: UIButton) {
+        if tfNombre.text != "" {
+            ref?.child("webs").childByAutoId().setValue(tfNombre.text)
+            var s = "http://"
+            s += tfNombre.text!
+            webView.loadRequest(URLRequest(url: URL(string: s)!))
+        }
+    }
+    
+    @objc func tfAction(_ textField: UITextField) {
+        if webView.canGoBack {
+            bAtras.isEnabled=true
+            
+        }
+        else {
+            bAtras.isEnabled=false
+        }
+        
+        if webView.canGoForward {
+            bSiguiente.isEnabled=true
+            
+        }
+        else {
+            bSiguiente.isEnabled=false
+        }
+        
+        if (tfNombre.text?.characters.count)! > 0 {
+            bAdd.isEnabled=true
+        }
+        else {
+            bAdd.isEnabled=false
+        }
+        
+    }
+    @IBAction func bSiguiente(_ sender: Any) {
+        webView.goForward()
+    }
+    
+    @IBAction func bAtras(_ sender: Any) {
+        webView.goBack()
+    }
+    
 }
+
 
